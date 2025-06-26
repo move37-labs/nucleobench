c380984d19aa575725dd48c9889b3d9f0518f880
Joel Shor <shor.joel@gmail.com>
2025-06-26 14:32:00 +0300

    Remove outdated install.sh script.

--- install.sh
+++
@@ -1,9 +0,0 @@
-# Installs necessary environment and dependencies on a clean machine.
-# Most useful for VMs.
-
-"${SHELL}" <(curl -L micro.mamba.pm/install.sh)
-source ~/.bashrc
-
-git clone https://github.com/joel-shor/nucleobench.git
-
-micromamba env create -f environment.yml
