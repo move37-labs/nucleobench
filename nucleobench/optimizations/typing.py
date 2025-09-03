043e3ff5dc992bea4b5970be125116713ff8cbfc
Joel Shor <shor.joel@gmail.com>
2025-09-02 22:58:14 -0400

    Fix typing to be more precise.

--- nucleobench/optimizations/typing.py
+++ nucleobench/optimizations/typing.py
@@ -1,12 +1,12 @@
 """Typing for NucleoBench"""
 
-from typing import Union
+from typing import Union, Optional
 
 from nucleobench.optimizations import model_class
 
 SequenceType = model_class.SequenceType
 SamplesType = list[SequenceType]
-PositionsToMutateType = list[int]
+PositionsToMutateType = Optional[list[int]]
 TISMType = list[dict[str, float]]
 
 ModelType = Union[model_class.ModelClass, callable]
