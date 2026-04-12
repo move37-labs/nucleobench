6c7c5bcff01439aaf42f8df3b3b684b39c9335ad
Joel Shor <shor.joel@gmail.com>
2026-04-11 20:12:58 -0400

    Use dotenv setup for secrets.

--- nucleobench/models/grelu/constants.py
+++ nucleobench/models/grelu/constants.py
@@ -1,5 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()  # loads .env if present; real env vars (e.g. GitHub Actions secrets) take precedence
+
 # Must be this, to be consistent with gRelu.
 VOCAB_ = ["A", "C", "G", "T"]
+
+WANDB_API_KEY_ = os.getenv("WANDB_API_KEY")
 
 # "Auto" device option.
 AUTO_DEVICE = "auto"
