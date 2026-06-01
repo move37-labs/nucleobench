052741c6fc846fd663e78c30bbba4a8276b2209a
Joel Shor <shor.joel@gmail.com>
2026-06-02 08:32:08 +0900

    Fix typing error from ruff.

--- nucleobench/optimizations/typing.py
+++ nucleobench/optimizations/typing.py
@@ -1,5 +1,7 @@
 """Typing for NucleoBench"""
 
+from collections.abc import Callable
+
 from nucleobench.optimizations import model_class
 
 SequenceType = model_class.SequenceType
@@ -7,6 +9,6 @@ SamplesType = list[SequenceType]
 PositionsToMutateType = list[int] | None
 TISMType = list[dict[str, float]]
 
-ModelType = model_class.ModelClass | callable
+ModelType = model_class.ModelClass | Callable
 TISMModelClass = model_class.TISMModelClass
 PyTorchDifferentiableModel = model_class.PyTorchDifferentiableModel
